92683567dadd235949c415675dce6b6e56eacdad
Anton <anton123098@gmail.com>
2021-04-17 18:47:31 +0300

    3

--- CarManager1/src/ru/com/anton/Car.java
+++ CarManager1/src/ru/com/anton/Car.java
@@ -1,5 +1,11 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package ru.com.anton;
 
 public class Car {
-    String name;
+    public Car() {
+    }
 }
--- CarManager1/src/ru/com/anton/CarManager.java
+++ CarManager1/src/ru/com/anton/CarManager.java
@@ -2,7 +2,7 @@ package ru.com.anton;
 
 public class CarManager {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         System.out.println("Car Manager !!!!!!!!!");
 
     }
